f4224b0829d0d2c80c34ab45c5d3286044cde6a5
Nyi Htet <nyihtet4dev@icloud.com>
2026-03-03 23:47:36 -0800

    Initial Commit

---
+++ audioMaster/audioMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  audioMasterApp.swift
+//  audioMaster
+//
+//  Created by Nyi Htet on 3/3/26.
+//
+
+import SwiftUI
+
+@main
+struct audioMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
